e7af62a1eb3c0dd933eca2f51045804caf88fbc7
coyupola <yuka@kamiyamayuka-no-MacBook-Air.local>
2016-07-28 23:11:35 +0900

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
- get 'posts' =>'posts#index'   #ホーム画面（ポスト一覧）
- get 'posts/new' => 'posts#new'   #投稿画面
- post 'posts' =>'posts#create'
+  devise_for :users
+  root to "top#index"
+  get 'posts' => 'posts#index'   #ホーム画面（ポスト一覧）
+  get 'posts/new' => 'posts#new'   #投稿画面
+  post 'posts' => 'posts#create'  
+  get 'users/:id' => 'users#show'
 end
